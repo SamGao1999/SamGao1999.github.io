b6eed80697ea4cfd8608ceed040cd8b76cbe85cd
SamGao1999 <77137767+SamGao1999@users.noreply.github.com>
2021-01-26 12:21:06 -0500

    Add files via upload

---
+++ functions/login.js
@@ -0,0 +1,37 @@
+var objPeople = [
+    {
+        username:"Sam",
+        password:"Qq81881664"
+    },
+    {
+        username:"Wilson",
+        password:"password"
+    },{
+        username:"Alex",
+        password:"xelA"
+    },{
+        username:"Guest",
+        password:"123"
+    },{
+        username:"Maycol",
+        password:"hahahajaja"
+    }
+]
+
+function getInfo(){
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+    var status = false;
+
+    for(i = 0; i < objPeople.length; i++){
+        if(username == objPeople[i].username && password == objPeople[i].password){
+            alert(username + " is logged in.")
+            status = true;
+        }
+    }
+
+    if(status==false){
+        alert("Login failed")
+    }
+
+}
